7fee660e1f40c680572c0541e5ec1d9fc85a471e
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-21 18:07:13 +0200

    ...

---
+++ SensiveBlog.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveBlog.BusinessLayer.Abstract;
+
+namespace SensiveBlog.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public DefaultController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IActionResult ArticleList()
+        {
+            var values = _articleService.ArticleListWithCategoryAndAppUser();
+            return View();
+        }
+    }
+}
